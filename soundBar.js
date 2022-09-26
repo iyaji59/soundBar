61bd7c8772815362665072f3e8059c6f8f1fa36f
Tonybruce <71387604+iyaji59@users.noreply.github.com>
2022-09-26 08:16:58 +0100

    Add files via upload

---
+++ soundBar.js
@@ -0,0 +1,26 @@
+const sounds = [
+    'Fem',
+    'Jowo',
+    'Soledad',
+    'Whatmakesaman',
+    'Foryou',
+    'Zoom',
+];
+sounds.forEach((sound) => {
+    const btn = document.createElement('button');
+    btn.classList.add('btn');
+    btn.innerText = sound;
+    btn.addEventListener('click', () => {
+        stopSound();
+        document.getElementById(sound).play();
+    });
+    document.getElementById('buttons')
+    .appendChild(btn);
+});
+function stopSound(){
+    sounds.forEach((sound) => {
+        const song = document.getElementById(sound)
+        song.pause();
+        song.currentTime = 0;
+    })
+}
